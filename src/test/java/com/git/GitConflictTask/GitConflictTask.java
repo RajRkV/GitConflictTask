c294dc05f6044274880f11279808c30aa16ddcdb
RajRkV <jackrk10@gmail.com>
2022-11-07 00:23:53 +0530

    Created new methods 1-3

---
+++ src/test/java/com/git/GitConflictTask/GitConflictTask.java
@@ -0,0 +1,13 @@
+package com.git.GitConflictTask;
+
+public class GitConflictTask {
+	public void method1() {
+		System.out.println("Method-1");
+	}
+	public void method2() {
+		System.out.println("Method-2");
+	}
+	public void method3() {
+		System.out.println("Method-3");
+	}
+}
