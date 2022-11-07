d4e88f6656bd1dac44b023325c9135d2cc96ec42
RajRkV <jackrk10@gmail.com>
2022-11-07 09:00:05 +0530

    Scripting Method7-Method9

--- src/test/java/com/git/GitConflictTask/GitConflictTask.java
+++ src/test/java/com/git/GitConflictTask/GitConflictTask.java
@@ -10,4 +10,13 @@ public class GitConflictTask {
 	public void method3() {
 		System.out.println("Method-3");
 	}
+	public void method7() {
+		System.out.println("Method-7");
+	}
+	public void method8() {
+		System.out.println("Method-8");
+	}
+	public void method9() {
+		System.out.println("Method-9");
+	}
 }
